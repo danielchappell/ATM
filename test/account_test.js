930bda69758e98db765cff321171dfa76b8e46e8
Joseph Chappell <jdchappe@gmail.com>
2014-04-25 17:24:23 -0500

    added failing test for Account retrieveBalance should be able to retrieve current balance

--- test/account_test.js
+++ test/account_test.js
@@ -6,13 +6,19 @@ expect = chai.expect;
 //TESTS FOR ACCOUNT.JS//
 describe("Account", function() {
   //CREATE NEW INSTANCE FOR EACH TEST//
-  var user = new Account(2000, 4385);
+  var user = new Account(2000, '4385');
   describe("constructor", function() {
     it("should initialize", function() {
       expect(user.constructor.name).to.equal("Account");
     });
     it("should accept initial deposit and pin", function() {
       expect(Account).to.have.length(2);
+    });
+  });
+  describe("retrieveBalance", function() {
+    it("should be able to retrieve current Balance", function() {
+      var balance = user.retrieveBalance('4385');
+      expect(balance).to.equal(2000);
     });
   });
 });

9963996c9c700a65fdbb423dbb51e4270461622d
Joseph Chappell <jdchappe@gmail.com>
2014-04-25 18:27:52 -0500

    added failing test for Account.setNewPin should be able to change pin

--- test/account_test.js
+++ test/account_test.js
@@ -5,9 +5,8 @@ expect = chai.expect;
 
 //TESTS FOR ACCOUNT.JS//
 describe("Account", function() {
-  //CREATE NEW INSTANCE FOR EACH TEST//
-  var user = new Account(2000, '4385');
   describe("constructor", function() {
+    var user = new Account(2000, '4385');
     it("should initialize", function() {
       expect(user.constructor.name).to.equal("Account");
     });
@@ -16,6 +15,7 @@ describe("Account", function() {
     });
   });
   describe("validatePin", function() {
+    var user = new Account(2000, '4385');
     it("should accurately validate provided pin", function() {
       var valid = user.validatePin("5000");
       expect(valid).to.equal(false);
@@ -23,7 +23,22 @@ describe("Account", function() {
       expect(valid).to.equal(true);
     });
   });
+  describe("setNewPin", function() {
+    var user = new Account(2000, '4385');
+    it("should be able to change pin", function() {
+      var newPin = '5555',
+      valid = user.validatePin(newPin);
+      //USE WRONG PIN EXPECT VALIDATION TO FAIL//
+      expect(valid).to.equal(false);
+      //SET NEW PIN USING CURRENT PIN//
+      user.setNewPin('4385', newPin);
+      // EXPECT NEW PIN TO NOW VALIDATE//
+      valid = user.validatePin(newPin);
+      expect(valid).to.equal(true);
+    });
+  });
   describe("retrieveBalance", function() {
+    var user = new Account(2000, '4385');
     it("should be able to retrieve current Balance", function() {
       var balance = user.retrieveBalance('4385');
       expect(balance).to.equal(2000);

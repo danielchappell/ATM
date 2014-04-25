9dc047c88e3f3fea4abe91b9e1c2dbb49a8adab5
Joseph Chappell <jdchappe@gmail.com>
2014-04-25 16:27:15 -0500

    added failing test newAccount should be able to create new account

--- test/atm_test.js
+++ test/atm_test.js
@@ -8,15 +8,22 @@ chai.should()
 
 describe("ATM", function() {
   //CREATE NEW INSTANCE FOR EACH TEST//
-  var atm = new ATM();
-
+  var atm = new ATM(),
+  user = null;
   describe('constructor', function() {
     it("should initialize", function() {
       expect(atm.constructor.name).to.equal("ATM");
     });
     it('should have accounts property', function() {
       var isArray = Array.isArray(atm.accounts);
+      expect(atm).to.have.property("accounts")
       expect(isArray).to.equal(true);
+    });
+  });
+  describe("newAccount", function() {
+    it("should be able to create new account", function() {
+      user = atm.newAccount(1000, 5577);
+      expect(user.constructor.name).to.equal("Account");
     });
   });
 });

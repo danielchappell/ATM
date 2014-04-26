ae11227b2642c467f7f08c91648eec87a39a7056
Joseph Chappell <jdchappe@gmail.com>
2014-04-25 22:54:04 -0500

    added ATM.newAccount adds to accounts array and assigns and returns account number

--- atm.js
+++ atm.js
@@ -1,11 +1,21 @@
+var Account, ATM;
+Account = require('./account.js');
 
 //ATM.JS//
 
-var ATM = (function() {
-  //Constructor Function//
+ATM = (function() {
   function ATM() {
+    //PRIVATE BANK KEY//
+    var bankID = Math.floor(Math.random() * 1000000000000000)
     // ATM PROPERTIES//
     this.accounts = [];
+    //CREATES SECURE NEW BANK ACCOUNTS//
+    this.newAccount = function(initDeposit, initPin) {
+      var newAccount = new Account(initDeposit, initPin);
+      this.accounts.push(newAccount);
+      newAccount.accountNumber = this.accounts.length;
+      return newAccount.accountNumber;
+    };
   }
   return ATM
 })();

a2eb3268bb39b1c095851e584b9fe3e53ef580c8
Joseph Chappell <jdchappe@gmail.com>
2014-04-27 08:14:55 -0500

    added transaction menu and callback UI methods reworked to integrate

--- atm.js
+++ atm.js
@@ -1,15 +1,16 @@
-var Account, prompt, promptSchemas, ATM;
+var Account, prompt, promptSchemas, colors, ATM;
 Account = require('./account.js');
 promptSchemas = require('./prompt_schemas.js');
 prompt = require('prompt');
+colors = require('colors');
 
 //ATM.JS//
 
 ATM = (function() {
   function ATM() {
     //PRIVATE KEYS AND DATA//
-    var bankID, session, sessionPin, defaultScreenCallback, userRegistrationCallback,
-     anotherTransactionCallback, clearScreen;
+    var bankID, session, sessionPin, defaultScreenCallback,userRegistrationCallback, transactionMenu,
+      transactionMenuCallback, promptAnotherTransaction, anotherTransactionCallback, clearScreen;
     bankID = Math.floor(Math.random() * 1000000000000000).toString(10);
     //FORMATTING FOR PROMPTS//
     prompt.start();
@@ -26,7 +27,8 @@ ATM = (function() {
     };
 
 
-    //UI CALLBACK METHODS//
+    //UI TERMINAL PROMPT METHODS//
+    //PERHAPS TEMPORARY//
     defaultScreenCallback = function(err, choice) {
       clearScreen();
       if (err) {return}
@@ -51,21 +53,66 @@ ATM = (function() {
       var initDeposit = credentials["initial deposit"],
       initPin = credentials["secure pin"],
       accountNumber = this.newAccount(initDeposit, initPin);
-      this.startSession( err, {"account number": accountNumber, "pin": initPin} );
+      this.startSession( err, {"account number": accountNumber, "pin": initPin}, true );
+    };
+
+    promptAnotherTransaction = function(){
+      console.log("\n\n\n\n");
       prompt.get( promptSchemas.anotherTransaction, anotherTransactionCallback.bind(this) );
     };
 
     anotherTransactionCallback = function(err, choice) {
-      clearScreen();
       if (err) {return}
-        if ( choice["another transaction?"] === "yes" || choice["another transaction?"] === "y" ) {
-          //MAIN TRANSACTION PROMPT
-        }
-        else {
-          //ENDS SESSION AND RESETS FOR NEXT USER//
-          this.endSession();
-        }
-    }
+      if ( choice["another transaction?"] === "yes" || choice["another transaction?"] === "y" ) {
+        transactionMenu.call(this);
+      }
+      else {
+        //ENDS SESSION AND RESETS FOR NEXT USER//
+        // console.log(this);
+        this.endSession();
+      }
+    };
+
+    transactionMenu = function() {
+      clearScreen();
+      console.log(promptSchemas["transactionMenu"]["properties"]["transaction menu"]["menu"])
+      prompt.get( promptSchemas.transactionMenu, transactionMenuCallback.bind(this) );
+    };
+
+
+
+    transactionMenuCallback = function(err, choice) {
+      if (err) {return}
+      //SLIGHT DELAY AFTER TRANSACTION BEFORE PROMPTING FOR ANOTHER TRANSACTION//
+      var promptTimeOut = setTimeout(promptAnotherTransaction.bind(this), 1500);
+      console.log("\n\n")
+      switch (choice["transaction menu"]) {
+        case "1":
+            //CHECK BALANCE//
+            var balance = "your balance is:  $" + this.checkBalance();
+            console.log(balance.blue);
+            promptAnotherTransaction.call(this);
+            clearTimeout(promptTimeOut);
+          break;
+        case "2":
+          //PRINT ACCOUNT LEDGER//
+          this.printLedger();
+          promptAnotherTransaction.call(this);
+          clearTimeout(promptTimeOut);
+          break;
+        case "3":
+          //CHANGE PIN NUMBER//
+
+          break;
+        case "4":
+          //WITHDRAW FUNDS//
+
+          break;
+        case "5":
+          //DEPOSIT FUNDS//
+      }
+
+    };
 
     //PUBLIC && TESTABLE METHODS//
 
@@ -86,7 +133,7 @@ ATM = (function() {
       this.atmStatus = "LISTENING";
     };
     //STARTS BANKING SESSION BY VERIFYING USER//
-    this.startSession = function(err, credentials) {
+    this.startSession = function(err, credentials, newUser) {
       var accountNumber = credentials["account number"];
       //MAKE SURE ACCOUNT IS ON FILE//
       if ( this.accounts[accountNumber - 195342] instanceof Account) {
@@ -98,6 +145,14 @@ ATM = (function() {
           session = this.accounts[accountNumber - 195342];
           sessionPin = pin;
           this.atmStatus = "IN SESSION";
+          //NEWLY REGISTERED USERS ARE GIVEN THE OPTION TO LOG OUT//
+          //PREVENTS AUTO LOADING TRANSACTION MENU//
+          if (!newUser) {
+            transactionMenu.call(this);
+          }
+          else{
+            promptAnotherTransaction.call(this);
+          }
           return "session started";
         }
         else {
@@ -126,20 +181,27 @@ ATM = (function() {
 
     this.printLedger = function() {
       if (session) {
-        var unformattedLedger, formattedLedger, entryArray, date, string;
+        var unformattedLedger, formattedLedger,_i, _length, entryArray, date, string;
         unformattedLedger = session.retrieveLedger(sessionPin, bankID);
         formattedLedger = [];
-        //LOOP THROUGH FORMAT AND PRINT EACH ENTRY//
-        for ( var _i = 0, _length = unformattedLedger.length; _i < _length; _i++ ) {
-          entryArray = unformattedLedger[_i];
-          date = entryArray[0];
-          string = date.toLocaleDateString() + " "  +
-            date.toLocaleTimeString() + "    " +
-            entryArray[1] + "    " + entryArray[2] +
-            "  $" + entryArray[3].toFixed(2);
-          formattedLedger.push(string);
-          //PRINT EACH ENTRY TO THE SCREEN//
-          console.log(string);
+        _length = unformattedLedger.length;
+        if (_length > 0) {
+          //LOOP THROUGH FORMAT AND PRINT EACH ENTRY//
+          for ( _i = 0; _i < _length; _i++ ) {
+            entryArray = unformattedLedger[_i];
+            date = entryArray[0];
+            string = date.toLocaleDateString() + " "  +
+              date.toLocaleTimeString() + "    " +
+              entryArray[1] + "    " + entryArray[2] +
+              "  $" + entryArray[3].toFixed(2);
+            formattedLedger.push(string);
+            //PRINT EACH ENTRY TO THE SCREEN//
+            console.log(string.blue);
+          }
+        }
+        else {
+          //PRINT A MESSAGE IF LEDGER IS EMPTY//
+          console.log("no transactions yet!".blue);
         }
         return formattedLedger;
       }
